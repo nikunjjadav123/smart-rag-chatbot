c5db31ab8762b066b6da0010a84a8aee031f3aa3
Nikunj Jadav <dirensj@gmail.com>
2026-02-05 14:25:45 +0530

    refactor: rename RAG folder and remove obsolete files

--- rag-system/embeddings/hf_embeddings.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-from langchain_community.embeddings import HuggingFaceEmbeddings
-from config.settings import EMBEDDING_MODEL
-
-@st.cache_resource
-def load_embeddings():
-    return HuggingFaceEmbeddings(
-        model_name=EMBEDDING_MODEL
-    )
--- rag-system/utils/helpers.py
+++
@@ -1,9 +0,0 @@
-def build_context(docs):
-    return "\n\n".join(doc.page_content for doc in docs)
-
-def extract_page_numbers(docs):
-    pages = set()
-    for doc in docs:
-        if "page" in doc.metadata:
-            pages.add(doc.metadata["page"] + 1)
-    return sorted(pages)
